195930aa1fc4b035c635cb15be057a46cf675fdf
xbcbupt725 <xbcbupt725@gmail.com>
2016-10-17 20:07:22 +0800

    12. Integer to Roman solved.

---
+++ src/Solution012.java
@@ -0,0 +1,25 @@
+public class Solution012 {
+	public static void main(String[] args){
+		int num = 3999;
+		Solution012 s = new Solution012();
+		System.out.println(s.intToRoman(num));
+	}
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        final String[][] set ={ 
+        	{"","I","II","III","IV","V","VI","VII","VIII","IX","X"},
+        	{"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC","C"},
+        	{"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM","M"},
+        	{"","M","MM","MMM","","","","","","",""},
+        };
+        int m = num / 1000;
+        int h = (num % 1000) / 100;
+        int t = (num % 100) / 10;
+        int o = num % 10;
+        sb.append(set[3][m]);
+        sb.append(set[2][h]);
+        sb.append(set[1][t]);
+        sb.append(set[0][o]);
+        return sb.toString();
+    }
+}
